85d79cda972b12a8960bcf0f3e1b4d5c7dcdb1bf
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-05-15 20:26:29 -0300

    Optimize gene pools.
      Now the gene pool uses a proper shuflle algorithm and doesn't take 60%
      of cpu time anymore.

--- openMP/GA/pools.h
+++ openMP/GA/pools.h
@@ -4,18 +4,19 @@
 #include "config.h"
 #include "population.h"
 
-typedef unsigned char * GenePool;
+typedef struct genepool * GenePool;
 
-GenePool pool_gene_new(void); //creates new pool filled with trues
-void pool_gene_destroy(GenePool); //destroys pool
+GenePool gp_new(void); //creates new pool filled with trues
+void gp_destroy(GenePool); //destroys pool
 
 //confirms if this gene hasn't been used
-#define pool_gene_usable(gp, g) (((unsigned char)1 << g%8 & gp[g/8]) != 0)
-//confirms if this gene has been used
-#define pool_gene_unusable(gp, g) (((unsigned char)1 << g%8 & gp[g/8]) == 0)
+int gp_usable(GenePool gp, gene_t g);
 
-void pool_gene_reset(GenePool); //resets UNDESTROYED pool
-void pool_gene_remove(GenePool, gene_t); //removes gene from pool
-gene_t pool_gene_get_random(GenePool); //returns random gene and removes it from pool
+//confirms if this gene has been used
+int gp_unusable(GenePool gp, gene_t g);
+
+void gp_reset(GenePool); //resets UNDESTROYED pool
+void gp_remove(GenePool, gene_t); //removes gene from pool
+gene_t gp_get_random(GenePool); //returns random gene and removes it from pool
 
 #endif
