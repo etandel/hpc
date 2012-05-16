90d2478cf54d00557896b1e8bc3cf38b84895ee9
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-05-15 21:06:28 -0300

    Add command line arguments to both versions.

--- openMP/SA/par_main.c
+++ openMP/SA/par_main.c
@@ -6,12 +6,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <string.h>
 
 static int should_replace(Tour * new, Tour * old, double temperature){
     // returns true if dl < 0 (new path is better than old)
     // or randomly, based on boltzman probability distribution
     double dl = new->length - old->length;
     return dl<0 ? 1 : rand()/RAND_MAX < exp(-dl/temperature);
+}
+
+static int read_args(const int argc, const char *argv[], int *verb, int *nthreads){
+    int i;
+    int v = 0, n = 8;
+    if (argc == 0) return 0;
+    for (i=0; i<argc; i++){
+        if (strcmp(argv[i], "-v") == 0)
+            v = 1;
+        else if (strcmp(argv[i], "-t") == 0){
+            //-t without any arguments after or with no following integer
+            if ((argc == i+1) || (sscanf(argv[i+1], "%d", &n) == 0))
+                return 0;
+        }
+
+    }
+    *verb = v;
+    *nthreads = n;
+    return 1;
 }
 
 int main(const int argc, const char *argv[]){
@@ -25,8 +45,16 @@ int main(const int argc, const char *argv[]){
 
     TownList * towns;
     Tour *old_tour, *new_tour;
-    int i = 0;
+    int i = 0, verbose, nthreads;
     double start;
+
+    if (!read_args(argc, argv, &verbose, &nthreads)){
+        puts("Bad argument list. Exiting...");
+        return 1;
+    }
+
+    if (verbose)
+        printf("Executing parallel version with %d threads.\n", nthreads);
 
     srand((int)time(NULL));
 
@@ -48,8 +76,9 @@ int main(const int argc, const char *argv[]){
     }
     
 
-    printf("Total time: %f\n", omp_get_wtime() - start);
-    printf("After %d iterations, the best length: %f\n", i, tour_length(old_tour));
+    printf("Sequential time: %f\n", omp_get_wtime() - start);
+    if (verbose)
+        printf("After %d iterations, the best length: %f\n", i, tour_length(old_tour));
     tour_destroy(old_tour);
     tl_destroy(towns);
     return 0;
