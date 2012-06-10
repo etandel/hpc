6b195b1914eba347b50e526f1728cf854560f3a6
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-10 04:39:44 -0300

    Remove useless arguments. Change functions and header to MPI.

--- MPI/SA/seq_main.c
+++ MPI/SA/seq_main.c
@@ -5,33 +5,9 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <omp.h>
+#include <MPI.h>
 #include <string.h>
 
-static struct args {
-    int verbose;
-    int nvertex;
-};
-
-static int read_args(const int argc, const char *argv[], struct args * args){
-    int i;
-    int v = 0, nvertex=NUM_VERTEXES;
-    if (argc == 0) return 0;
-    for (i=0; i<argc; i++){
-        if (strcmp(argv[i], "-v") == 0)
-            v = 1;
-        else if (strcmp(argv[i], "-n") == 0){
-            //-t without any arguments after or with no following integer
-            if ((argc == i+1) || (sscanf(argv[i+1], "%d", &nvertex) == 0))
-                return 0;
-        }
-        
-
-    }
-    args->verbose = v;
-    args->nvertex = nvertex;
-    return 1;
-}
 
 static int should_replace(Tour * new, Tour * old, double temperature){
     // returns true if dl < 0 (new path is better than old)
@@ -42,7 +18,6 @@ static int should_replace(Tour * new, Tour * old, double temperature){
 
 
 int main(const int argc, const char *argv[]){
-    struct args args;
     Config state = {
         GRID_SIZE,
         NUM_VERTEXES,
@@ -53,21 +28,14 @@ int main(const int argc, const char *argv[]){
 
     TownList * towns;
     Tour *old_tour, *new_tour;
-    int i = 0, verbose, nthreads;
+    int i = 0;
     double start;
 
-    if (!read_args(argc, argv, &args)){
-        puts("Bad argument list. Exiting...");
-        return 1;
-    }
-
-    state.num_vertexes = args.nvertex;
-    if (args.verbose)
-        puts("Executing sequential version");
+    //puts("Executing sequential version");
 
     srand((int)time(NULL));
 
-    start = omp_get_wtime();
+    start = MPI_Wtime();
     towns = tl_new(state);
     old_tour = tour_new(towns); 
     while (state.temperature > state.epsilon){
@@ -85,9 +53,8 @@ int main(const int argc, const char *argv[]){
     }
     
 
-    printf("Sequential time: %f\n", omp_get_wtime() - start);
-    if (args.verbose)
-        printf("After %d iterations, the best length: %f\n", i, tour_length(old_tour));
+    printf("Sequential time: %f\n", MPI_Wtime() - start);
+    //printf("After %d iterations, the best length: %f\n", i, tour_length(old_tour));
     tour_destroy(old_tour);
     tl_destroy(towns);
     return 0;
