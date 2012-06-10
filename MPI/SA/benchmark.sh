6912528b9682e7697e63ee0f4989cac92e88f8d3
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-06-10 20:17:39 -0300

    Update benchmark script.

--- MPI/SA/benchmark.sh
+++ MPI/SA/benchmark.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 ntests=10
-param_nvertex=
 
 ############ Sequential ###########
 echo -e "Executanto versao sequencial $ntests vezes.\n"
 seq_time=0
 for i in $(seq $ntests); do
-    output=$(./sequential $param_nvertex)
+    output=$(./sequential)
     echo $output
     seq_time=$seq_time+$(echo $output | cut -d' ' -f3)
 done
@@ -16,10 +15,10 @@ echo -e "Total time: $seq_time"
 
 
 ############ Parallel 8 ###########
-echo -e "\nExecutanto versao paralela com 8 threads $ntests vezes."
+echo -e "\nExecutanto versao paralela com 8 processos $ntests vezes."
 par8_time=0
 for i in $(seq $ntests); do
-    output=$(./parallel -t 8 $param_nvertex)
+    output=$(./parallel)
     echo $output
     par8_time=$par8_time+$(echo $output | cut -d' ' -f3)
 done
@@ -29,10 +28,10 @@ echo "Speedup 8: $(echo -e "scale=10\n"$seq_time/$par8_time | bc)"
 
 
 ############ Parallel 4 ###########
-echo -e "\nExecutanto versao paralela com 4 threads $ntests vezes."
+echo -e "\nExecutanto versao paralela com 4 processos $ntests vezes."
 par4_time=0
 for i in $(seq $ntests); do
-    output=$(./parallel -t 4 $param_nvertex)
+    output=$(./parallel)
     echo $output
     par4_time=$par4_time+$(echo $output | cut -d' ' -f3)
 done
