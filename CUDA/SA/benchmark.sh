fc36c7872be6376219bc3e49b60235faf929d198
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-07-02 03:30:09 -0300

    Change benchmark script for CUDA.

--- CUDA/SA/benchmark.sh
+++ CUDA/SA/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ntests=10
+ntests=4
 
 ############ Sequential ###########
 echo -e "Executanto versao sequencial $ntests vezes.\n"
@@ -14,27 +14,14 @@ seq_time=$(echo $seq_time | bc)
 echo -e "Total time: $seq_time"
 
 
-############ Parallel 8 ###########
-echo -e "\nExecutanto versao paralela com 8 processos $ntests vezes."
-par8_time=0
+############ Parallel ###########
+echo -e "\nExecutanto versao paralela $ntests vezes."
+par_time=0
 for i in $(seq $ntests); do
-    output=$(mpiexec -n 8 ./parallel)
+    output=$(./parallel)
     echo $output
-    par8_time=$par8_time+$(echo $output | cut -d' ' -f3)
+    par_time=$par_time+$(echo $output | cut -d' ' -f3)
 done
-par8_time=$(echo $par8_time | bc)
-echo "Total time: $par8_time"
-echo "Speedup 8: $(echo -e "scale=10\n"$seq_time/$par8_time | bc)"
-
-
-############ Parallel 4 ###########
-echo -e "\nExecutanto versao paralela com 4 processos $ntests vezes."
-par4_time=0
-for i in $(seq $ntests); do
-    output=$(mpiexec -n 4 ./parallel)
-    echo $output
-    par4_time=$par4_time+$(echo $output | cut -d' ' -f3)
-done
-par4_time=$(echo $par4_time | bc)
-echo "Total time: $par4_time"
-echo "Speedup 4: $(echo -e "scale=10\n"$seq_time/$par4_time | bc)"
+par_time=$(echo $par_time | bc)
+echo "Total time: $par_time"
+echo "Speedup: $(echo -e "scale=10\n"$seq_time/$par_time | bc)"
